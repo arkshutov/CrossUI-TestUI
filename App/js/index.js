c1931d638f9a6cc387aaf372b949e3a7317dcc65
arkshutov <78549724+arkshutov@users.noreply.github.com>
2023-02-20 17:52:14 +0300

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -56,7 +56,7 @@ xui.Class('App', 'xui.Module',{
                         "target" : "msg",
                         "args" : [
                             "Hello",
-                            "Hello, {page.xui_ui_input5}"
+                            "Hello, {page.xui_ui_input5.getValue(  )}"
                         ],
                         "method" : "pop",
                         "okFlag" : "_confirm_yes",
